e1abde3fb2dfdcb0c82cc83ecce2ac2014b29b27
amaalyy <amal.elzowawi@gmail.com>
2024-01-26 19:30:52 +0100

    Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import { createUser, uploadPhoto } from "./utils";
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-	return Promise.all([uploadPhoto(), createUser()])
-	.then((values) => {
-		const { body } = values[0];
-		const { firstName } = values[1];
-		const { lastName } = values[1];
-		console.log('${body} ${firstName} ${lastName}');
-	})
-	.catch(() => console.log('Signup system offline'))
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
