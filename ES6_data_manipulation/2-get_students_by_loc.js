37e805ac5008cb3510df13689256c5c2615fc6c8
amaalyy <amal.elzowawi@gmail.com>
2024-01-29 21:32:31 +0100

    Filter

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,9 @@
 export default function getStudentsByLocation(students, city) {
-	if (!Array.isArray(students)) {
-		return [];
-	}
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-	const result = students.filter((item) => item.location === city);
+  const result = students.filter((item) => item.location === city);
 
-	return result;
+  return result;
 }
