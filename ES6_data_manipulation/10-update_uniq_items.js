0a4e91be5b4883904eadda386726f8eab0a34ada
amaalyy <amal.elzowawi@gmail.com>
2024-01-31 01:58:37 +0100

    More map data structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(map) {
-	if (!(map instanceof Map)) {
-		throw Error('Cannot process');
-	}
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
 
-	for (const [key] of map) {
-		if (map.get(key) === 1) map.set(key, 100);
-	}
+  for (const [key] of map) {
+    if (map.get(key) === 1) map.set(key, 100);
+  }
 
-	return map;
+  return map;
 }
