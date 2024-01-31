cc5097ffa9120e6de210740bcf675dc96897e1b2
amaalyy <amal.elzowawi@gmail.com>
2024-01-31 01:40:56 +0100

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,9 @@ export default function cleanSet(set, startString) {
   const string = [];
 
   if (
-    typeof set !== 'object' ||
-    typeof startString !== 'string' ||
-    startString.length === 0
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
   ) {
     return '';
   }
