b6c54f02748cb82bcb342cd2799bd8ac28eb1c65
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-06-27 21:40:53 +0100

    User model

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -9,7 +9,7 @@ class User():
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), unique=True)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=False)
-    reset_token = Column(String(255), nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
