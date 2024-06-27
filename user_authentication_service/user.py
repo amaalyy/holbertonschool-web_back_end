a062a6e4c57850687933c0882285140c42d2852d
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-06-27 21:36:20 +0100

    User model

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+""" SQLAlchemy model """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
-class User(Base):
+# Define the User class
+class User():
+    """ SQLAlchemy model """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    email = Column(String(255), unique=True)
+    hashed_password = Column(String(255), nullable=False)
+    session_id = Column(String(255), nullable=False)
+    reset_token = Column(String(255), nullable=False)
