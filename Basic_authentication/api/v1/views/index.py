1d0f38e2a5a75e55f0e9866c372bfd6cc1b32426
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-06-27 20:48:59 +0100

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
 from flask import jsonify, abort
 from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -8,6 +12,7 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -20,10 +25,18 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - 401 Unauthorized error
+    """GET /api/v1/unauthorized
+    This endpoint will raise a 401 error using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    This endpoint will raise a 403 error using abort
+    """
+    abort(403)
