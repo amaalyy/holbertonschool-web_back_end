8e25e6c319918c1e678d2ad68ea0a0477bad27c5
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-06-27 21:44:48 +0100

    User model

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ SQLAlchemy model """
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-# Define the User class
-class User():
+class User(Base):
     """ SQLAlchemy model """
     __tablename__ = 'users'
 
