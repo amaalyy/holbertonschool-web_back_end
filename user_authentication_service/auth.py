03c2792ceecdd5f2071558af94739bb622561ae2
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-06-28 18:40:31 +0100

    Hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hashes a password with a randomly-generated salt
+        and returns the salted,   password.
+        """
+        salt = bcrypt.gensalt()
+
+        hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers a user with a given email and password.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
