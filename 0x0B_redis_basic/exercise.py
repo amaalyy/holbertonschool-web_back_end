50ef473b4930659e756a0b314d97f825c6b471c0
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-08-08 19:39:34 +0100

    Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,21 +2,15 @@
 """
 exercic file
 """
-
 from typing import Union
 from uuid import uuid4, UUID
-
 import redis
-
-
 class Cache:
     """ Class for implementing a Cache """
-
     def __init__(self):
         """ Constructor Method """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis using a
         random key and return the key.
@@ -25,3 +19,27 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
+    def get (self, key, data: Union[str, bytes, int, float])-> str:
+        """
+        store input data in dedis using a random key
+        and return a key
+        """
+        value = self._redis.get(key)
+
+        return value
+    def get_str(self, key: str) -> str:
+        """
+        param a value from redis to str
+        """
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+    def get_int(self, key: int) -> int:
+        """
+        param a value from redis to int
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
