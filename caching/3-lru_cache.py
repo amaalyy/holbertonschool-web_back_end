42767579137cef93f8e6b72f2cd070fbcbea6be3
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-11-10 16:35:07 +0100

    fix

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,41 +1,34 @@
-#!/usr/bin/python3
-"""
-this is the main class
-"""
-from collections import OrderedDict
-BaseCaching = __import__('base_caching').BaseCaching
+#!/usr/bin/env python3
+"""LRU caching module"""
+
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    '''
-    LRUCache class
-    __init__(): Initializes the LRUCache object.
-    put: Adds an item to the cache.
-    get: Retrieves an item from the cache based on the key.
-    '''
+    """LRU caching system"""
 
     def __init__(self):
-        """
-        this is the initialization method
-        """
+        """initialize"""
         super().__init__()
-        self.access_order = OrderedDict()
+        self.cache_queue = []
 
     def put(self, key, item):
-        """
-        this is the put method
-        """
+        """add an item in the cache"""
         if key is not None and item is not None:
-            # Check if the cache is full
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Discard the least recently used item (LRU)
-                lru_key = next(iter(self.access_order))
-                del self.access_order[lru_key]
-                del self.cache_data[lru_key]
-                print(f"DISCARD: {lru_key}")
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                discarded_key = self.cache_queue.pop(0)
+                del self.cache_data[discarded_key]
+                print("DISCARD:", discarded_key)
             self.cache_data[key] = item
-            self.access_order[key] = None
+            if key in self.cache_queue:
+                self.cache_queue.remove(key)
+            self.cache_queue.append(key)
 
     def get(self, key):
-        """Get an item by key"""
-        return self.cache_data.get(key) if key is not None else None
+        """get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        if key in self.cache_queue:
+            self.cache_queue.remove(key)
+        self.cache_queue.append(key)
+        return self.cache_data[key]
